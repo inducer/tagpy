fde564890ea7bc56031f67662093931b598892af
Andreas Kloeckner <mathem@tiker.net>
2006-01-08 21:48:16 +0000

    [tagpy @ Arch-1:inform@tiker.net--2005-public%tagpy--mainline--0--patch-7] Additional package metadata

--- setup.py
+++ setup.py
@@ -20,9 +20,27 @@ EXTRA_COMPILE_ARGS = []
 
 setup(name="tagpy",
       version="0.90.1",
-      description="TagLib bindings",
+      description="Bindings for TagLib",
+      long_description="Binds Python to Scott Wheeler's TagLib. " +
+      "Makes it easy to read ID3 tags from MP3 files, "+
+      "Xiph Comments from Ogg Vorbis and FLAC files, "+
+      "and APE tags from MusePack files.",
       author=u"Andreas Kloeckner",
       author_email="inform@tiker.net",
+      classifiers=
+      [ "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Operating System :: POSIX",
+        "Operating System :: Unix",
+        "Programming Language :: Python",
+        "Topic :: Multimedia :: Sound/Audio",
+        "Topic :: Multimedia :: Sound/Audio :: CD Audio :: CD Ripping",
+        "Topic :: Multimedia :: Sound/Audio :: Editors",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities"],
       license = "BSD-Style",
       url="http://news.tiker.net/software/tagpy",
       packages=["tagpy"],
