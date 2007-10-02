fd9f23bd1d7c3d8ab88d3ce53fe0951473b44905
Andreas Kloeckner <inform@tiker.net>
2007-10-02 10:22:08 -0400

    Fix tagrename to be exception-tolerant.
    Add gitignore.

--- setup.py
+++ setup.py
@@ -8,9 +8,9 @@ TAGLIB_INCLUDE_DIRS = ["/usr/include/taglib"]
 TAGLIB_LIBRARY_DIRS = []
 TAGLIB_LIBRARIES = ["tag"]
 
-BOOST_INCLUDE_DIRS = []
-BOOST_LIBRARY_DIRS = []
-BOOST_LIBRARIES = ["boost_python"]
+BOOST_INCLUDE_DIRS = ["/home/andreas/pool/include/boost-1_35"]
+BOOST_LIBRARY_DIRS = ["/home/andreas/pool/lib"]
+BOOST_LIBRARIES = ["boost_python-gcc42-mt"]
 # END USER CUSTOMIZABLE SECTION ----------------------
 
 INCLUDE_DIRS = TAGLIB_INCLUDE_DIRS + BOOST_INCLUDE_DIRS
