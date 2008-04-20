21d7da32c1dec68923b9d0b5dd4472eff38f8a8f
Andreas Kloeckner <inform@tiker.net>
2008-04-20 12:48:44 -0400

    Bump version. Update setup.py metadata.

--- setup.py
+++ setup.py
@@ -32,12 +32,26 @@ def main():
     EXTRA_COMPILE_ARGS = []
 
     setup(name="tagpy",
-          version="0.93",
-          description="Bindings for TagLib",
-          long_description="Binds Python to Scott Wheeler's TagLib. " +
-          "Makes it easy to read ID3 tags from MP3 files, "+
-          "Xiph Comments from Ogg Vorbis and FLAC files, "+
-          "and APE tags from MusePack files.",
+          version="0.94",
+          description="Python Bindings for TagLib",
+          long_description="
+          TagPy is a set of Python bindings for Scott Wheeler's 
+          `TagLib <http://developer.kde.org/~wheeler/taglib.html>`_. 
+          It builds upon `Boost.Python <http://www.boost.org/libs/python/doc/>_, 
+          a wrapper generation library which is part of the renowned Boost 
+          set of C++ libraries.
+
+          Just like TagLib, TagPy can:
+
+          * read and write ID3 tags of version 1 and 2, with many supported frame types
+            for version 2 (in MPEG Layer 2 and MPEG Layer 3, FLAC and MPC),
+          * access Xiph Comments in Ogg Vorbis Files and Ogg Flac Files,
+          * access APE tags in Musepack and MP3 files.
+
+          All these features have their own specific interfaces, but 
+          TagLib's generic tag reading and writing mechanism is also 
+          supported. It comes with a bunch of examples.
+          ",
           author=u"Andreas Kloeckner",
           author_email="inform@tiker.net",
           classifiers=
