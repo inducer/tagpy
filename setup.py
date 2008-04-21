d5142cfe7e3a564162b7e2f36b38233307e1d1b7
Andreas Kloeckner <inform@tiker.net>
2008-04-21 03:51:09 -0400

    Upgrade to aksetup.

--- setup.py
+++ setup.py
@@ -1,38 +1,40 @@
 #!/usr/bin/env python
 # -*- coding: latin-1 -*-
 
-from distutils.core import setup,Extension
+def get_config_schema():
+    from aksetup_helper import ConfigSchema, Option, \
+            IncludeDir, LibraryDir, Libraries, \
+            Switch, StringListOption
+
+    return ConfigSchema([
+        IncludeDir("BOOST", []),
+        LibraryDir("BOOST", []),
+        Libraries("BOOST_PYTHON", ["boost_python-gcc42-mt"]),
+
+        IncludeDir("TAGLIB", []),
+        LibraryDir("TAGLIB", []),
+        Libraries("TAGLIB", ["tag"]),
+
+        StringListOption("CXXFLAGS", [], 
+            help="Any extra C++ compiler options to include"),
+        ])
+
+
 
 
 def main():
-    from distutils.core import setup, Extension
-    from distutils import sysconfig
+    import glob
+    from aksetup_helper import hack_distutils, get_config, setup, Extension
 
-    def non_matching_config():
-        print "*** The version of your configuration template does not match"
-        print "*** the version of the setup script. Please re-run configure."
-        sys.exit(1)
+    hack_distutils()
+    conf = get_config()
 
-    try:
-        conf = {}
-        execfile("siteconf.py", conf)
-    except:
-        print "*** Please run configure first."
-        sys.exit(1)
-
-    if "TAGPY_CONF_TEMPLATE_VERSION" not in conf:
-        non_matching_config()
-
-    if conf["TAGPY_CONF_TEMPLATE_VERSION"] != 1:
-        non_matching_config()
-
-    INCLUDE_DIRS = conf["TAGLIB_INCLUDE_DIRS"] + conf["BOOST_INCLUDE_DIRS"]
-    LIBRARY_DIRS = conf["TAGLIB_LIBRARY_DIRS"] + conf["BOOST_LIBRARY_DIRS"]
-    LIBRARIES = conf["TAGLIB_LIBRARIES"] + conf["BOOST_LIBRARIES"]
-    EXTRA_COMPILE_ARGS = []
+    INCLUDE_DIRS = conf["TAGLIB_INC_DIR"] + conf["BOOST_INC_DIR"]
+    LIBRARY_DIRS = conf["TAGLIB_LIB_DIR"] + conf["BOOST_LIB_DIR"]
+    LIBRARIES = conf["TAGLIB_LIBNAME"] + conf["BOOST_PYTHON_LIBNAME"]
 
     setup(name="tagpy",
-          version="0.94",
+          version="0.94.1",
           description="Python Bindings for TagLib",
           long_description="""
           TagPy is a set of Python bindings for Scott Wheeler's 
@@ -69,17 +71,18 @@ def main():
             "Topic :: Software Development :: Libraries :: Python Modules",
             "Topic :: Utilities"],
           license = "BSD-Style",
-          url="http://news.tiker.net/software/tagpy",
+
+          url="http://mathema.tician.de/software/tagpy",
           packages=["tagpy", "tagpy.ogg"],
           package_dir={"tagpy": "src/python"},
           ext_modules=[ Extension("_tagpy", 
                                   ["src/wrapper/basics.cpp", 
                                    "src/wrapper/id3.cpp",
                                    "src/wrapper/rest.cpp"],
-                                  include_dirs = INCLUDE_DIRS,
-                                  library_dirs = LIBRARY_DIRS,
-                                  libraries = LIBRARIES,
-                                  extra_compile_args = EXTRA_COMPILE_ARGS,
+                                  include_dirs=INCLUDE_DIRS,
+                                  library_dirs=LIBRARY_DIRS,
+                                  libraries=LIBRARIES,
+                                  extra_compile_args=conf["CXXFLAGS"],
                                   ),
                         ]
           )
@@ -88,25 +91,4 @@ def main():
 
 
 if __name__ == '__main__':
-    # hack distutils.sysconfig to eliminate debug flags
-    # stolen from mpi4py
-    import sys
-    if not sys.platform.lower().startswith("win"):
-        from distutils import sysconfig
-
-        cvars = sysconfig.get_config_vars()
-        cflags = cvars.get('OPT')
-        if cflags:
-            cflags = cflags.split()
-            for bad_prefix in ('-g', '-O', '-Wstrict-prototypes'):
-                for i, flag in enumerate(cflags):
-                    if flag.startswith(bad_prefix):
-                        cflags.pop(i)
-                        break
-                if flag in cflags:
-                    cflags.remove(flag)
-            cflags.append("-O3")
-            cvars['OPT'] = str.join(' ', cflags)
-            cvars["CFLAGS"] = cvars["BASECFLAGS"] + " " + cvars["OPT"]
-    # and now call main
     main()
